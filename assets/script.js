f787b88d8c293ef7747192a3012d788e4e24abd1
GHBond007 <121248438+GHBond007@users.noreply.github.com>
2023-03-04 22:07:29 -0600

    Cleaner Code Commit

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,16 @@
 // Moment.js
 var currentDate = moment().format('dddd') + " " + moment().format("D MMM YYYY");
 var currentHour = moment().format('HH:mm:ss a');
-// hour and user input var
-
-var nineAm = $("09:00 hours");
-var tenAm = $("10:00 hours");
-var elevenAm = $("11:00 hours");
-var twelvePm = $("12:00 hours");
-var onePm = $("13:00 hours");
-var twoPm = $("14:00 hours");
-var threePm = $("15:00 hours");
-var fourPm = $("16:00 hours");
-var fivePm = $("17:00 hours");
+// Get references to the HTML elements for each hour block
+var timeBlock9 = $("09:00 hours");
+var timeBlock10 = $("10:00 hours");
+var timeBlock11 = $("11:00 hours");
+var timeBlock12 = $("12:00 hours");
+var timeBlock1 = $("13:00 hours");
+var timeBlock2 = $("14:00 hours");
+var timeBlock3 = $("15:00 hours");
+var timeBlock4 = $("16:00 hours");
+var timeBlock5 = $("17:00 hours");
 
 var hour = moment().hours();
 var userInput;
@@ -27,80 +26,69 @@ var interval = setInterval(function() {
                        .substring(0,3).toUpperCase());
   $('#currentDay').html(currentDate + " " + momentNow.format('HH:mm:ss A'));
 }, 100);
-//Initialize function to 
+//This function initializes the page by setting the values of each time block based on the user's saved input
 function initPage() {
-
+//Get the saved input for each time block from local storage and set the value of the corresponding input element
   console.log("Current Hour " + hour);
-  var init9 = JSON.parse(localStorage.getItem("09:00 am"));
-  nineAm.val(init9);
-
-  var init10 = JSON.parse(localStorage.getItem("10:00 am"))
-  tenAm.val(init10);
-  
-  var init11 = JSON.parse(localStorage.getItem("11:00 am"))
-  elevenAm.val(init11);
-  
-  var init12 = JSON.parse(localStorage.getItem("12:00 pm"))
-  twelvePm.val(init12);
-  
-  var init1 = JSON.parse(localStorage.getItem("01:00pm"))
-  onePm.val(init1);
-  
-  var init2 = JSON.parse(localStorage.getItem("02:00 pm"))
-  twoPm.val(init2);
-  
-  var init3 = JSON.parse(localStorage.getItem("03:00 pm"))
-  threePm.val(init3);
- 
-  var init4 = JSON.parse(localStorage.getItem("04:00 pm"))
-  fourPm.val(init4);
-  
-  var init5 = JSON.parse(localStorage.getItem("05:00 pm"))
-  fivePm.val(init5);
-  
+  timeBlock9.val(localStorage.getItem("09:00 am"));
+  timeBlock10.val(localStorage.getItem("10:00 am"));
+  timeBlock11.val(localStorage.getItem("11:00 am"));
+  timeBlock12.val(localStorage.getItem("12:00 pm"));
+  timeBlock1.val(localStorage.getItem("01:00 pm"));
+  timeBlock2.val(localStorage.getItem("02:00 pm"));
+  timeBlock3.val(localStorage.getItem("03:00 pm"));
+  timeBlock4.val(localStorage.getItem("04:00 pm"));
+  timeBlock5.val(localStorage.getItem("05:00 pm"));
   
 } 
+// This function sets the background color of each time block based on the current time
+function setBackground () {
+  // Get the current hour using Moment.js
+      hour = moment().hours();
 
-function background () {
-      
+      // Loop through each time block
   $(".text-line").each(function () {
-      var timeTest = parseInt($(this).attr("id"));
-      hour = parseInt(hour);
-      console.log(timeTest);
-      console.log(hour);
-//      console.log(this);
-      if (hour > timeTest) {
-          $(this).addClass("past");
-      } else if (hour < timeTest) {
-          $(this).addClass("future");
-      } else {
-          $(this).addClass("present");
-      }
+    // Get the hour for this time block by parsing the ID attribute
+    var timeBlockHour = parseInt($(this).attr("id"));
+  
+// Compare the current hour to the hour for this time block and set the appropriate class for the background color
+if (hour > timeBlockHour) {
+  $(this).addClass("past");
+} else if (hour < timeBlockHour) {
+  $(this).addClass("future");
+} else {
+  $(this).addClass("present");
+}
+      
   });
 }
-// Function to keep text in background
+
 $(document).ready(function(){
   initPage()
-  background()
+  setBackground()
 
-  // Buttons (save to Local Storage)
+  // This function saves the user's input to local storage when the corresponding save button is clicked
   $(".saveBtn").on("click", function(){
+    // Get the user's input and the corresponding hour for this time block
     userInput = $(this).siblings(".text-line").val().trim();
     console.log(userInput);
     hourSpan = $(this).siblings(".input-group-prepend").text().trim();
-    
+    console.log(hourSpan)
+    // Save the user's input to local storage
     localStorage.setItem(hourSpan, JSON.stringify(userInput));
 
   })
-  // Button for clear the day
-  $("#clearDay").on("click", function(){
-    localStorage.clear();
-    initPage()
-  }) 
 
 });
 
-  
+// Call the setBackground function to initialize the background color of each time block
+setBackground();
+
+// Call the initPage function to set the initial values of each time block based on saved input
+initPage();
+
+// Call the setBackground function again every minute to update the background color based on the current time
+setInterval(setBackground, 60000); 
 
 
 
